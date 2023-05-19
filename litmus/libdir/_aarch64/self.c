9330f809cd525caf095bf74479c44961b35a1323
Nikos Nikoleris <nikos.nikoleris@arm.com>
2023-05-19 17:22:26 +0100

    [litmus] Add support for variant self in KVM mode

--- litmus/libdir/_aarch64/self.c
+++ litmus/libdir/_aarch64/self.c
@@ -18,6 +18,22 @@
 /* Support for self-modifying code */
 /***********************************/
 
+static uint32_t getcachelinesize(void) {
+  uint64_t csz;
+  asm __volatile__ (
+                    "mrs %[r],CTR_EL0"
+                    :[r] "=&r" (csz)
+                    :
+                    :"cc","memory"
+                    );
+  uint64_t sz1 = csz & 0xf;
+  uint64_t sz2 = (csz >> 16 ) & 0xf;
+  uint32_t x = (uint32_t) (sz1 >= sz2 ? sz1 : sz2);
+  return (1 << x) * 4 ;
+}
+
+static uint32_t cache_line_size;
+
 inline static void selfbar(void *p) {
   asm __volatile__
     ("dc cvau,%[p]\n\t" "dsb ish\n\t" "ic ivau,%[p]\n\t" "dsb ish\n\t" "isb"
@@ -28,18 +44,43 @@ inline static void isync(void) {
   asm __volatile__ ("isb" ::: "memory");
 }
 
-static uint32_t cache_line_size;
+#ifdef KVM
 
-static uint32_t getcachelinesize(void) {
-  uint64_t csz;
-  asm __volatile__ (
-  "mrs %[r],CTR_EL0"
-:[r] "=&r" (csz)
-:
-:"cc","memory"
-    );
-  uint64_t sz1 = csz & 0xf;
-  uint64_t sz2 = (csz >> 16 ) & 0xf;
-  uint32_t x = (uint32_t) (sz1 >= sz2 ? sz1 : sz2);
-  return (1 << x) * 4 ;
+static void litmus_icache_sync(unsigned long vaddr, unsigned long vaddr_end)
+{
+  while (vaddr < vaddr_end) {
+    selfbar((void *)vaddr);
+    vaddr += cache_line_size;
+  }
 }
+
+static size_t code_size(ins_t *p,int skip) {
+  return (find_ins(getret(), p, skip) + 1) * sizeof(ins_t);
+}
+
+static void litmus_pte_unset_el0(unsigned long vaddr, unsigned long vaddr_end)
+{
+  while (vaddr < vaddr_end) {
+    pteval_t *pte = litmus_tr_pte((void *)vaddr);
+    litmus_set_pte_safe((void *)vaddr, pte, *pte & ~msk_el0);
+    vaddr += LITMUS_PAGE_SIZE;
+  }
+  asm __volatile__ (
+    "dsb ish\n\t"
+    "isb"
+    ::: "memory"
+  );
+}
+
+static void code_init(void *code, void *src, size_t sz)
+{
+  size_t sz_pages = (sz - 1) / LITMUS_PAGE_SIZE + 1;
+  memcpy(code, src, sz);
+  litmus_icache_sync((unsigned long)code, (unsigned long)code + sz);
+  /* Armv8 requires that code shared between EL1 and EL0 is read-only.
+   * We need write access and therefore, we have to ensure that the
+   * the memory we use for code won't be accessible from EL0 */
+  litmus_pte_unset_el0((unsigned long)code, (unsigned long)code + sz_pages);
+}
+
+#endif
